450e9e2b899b9630be54728a5227ba38c2a34bd3
Dana M. Diederich <diederich@gmail.com>
2015-11-15 13:50:21 -0800

    ported the basic tests from the reference implementation

--- messagematch_basic_test.go
+++ messagematch_basic_test.go
@@ -6,6 +6,174 @@ import (
 	"testing"
 )
 
+//ported from https://github.com/dana/perl-Message-Match/blob/master/t/basic.t
+//not nested
+func TestSimplestPossible(t *testing.T) {
+	pretty.Println("TestSimplestPossible")
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"a": "b",
+	}
+	match := map[string]interface{}{
+		"a": "b",
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.True(doesMatch)
+}
+func TestExtraStuff(t *testing.T) {
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"a": "b",
+		"c": "d",
+	}
+	match := map[string]interface{}{
+		"a": "b",
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.True(doesMatch)
+}
+func TestSimpleMiss(t *testing.T) {
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"a": "b",
+		"c": "d",
+	}
+	match := map[string]interface{}{
+		"e": "f",
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.False(doesMatch)
+}
+func TestSimplestPossibleMultiMatchRequired(t *testing.T) {
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"a": "b",
+		"c": "d",
+	}
+	match := map[string]interface{}{
+		"a": "b",
+		"c": "d",
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.True(doesMatch)
+}
+
+//nested
+func TestSimplestNested(t *testing.T) {
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"x": map[string]interface{}{
+			"y": "z",
+		},
+	}
+	match := map[string]interface{}{
+		"x": map[string]interface{}{
+			"y": "z",
+		},
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.True(doesMatch)
+}
+func TestSimplestNestedWithExtraStuff(t *testing.T) {
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"a": "b",
+		"x": map[string]interface{}{
+			"y": "z",
+		},
+	}
+	match := map[string]interface{}{
+		"x": map[string]interface{}{
+			"y": "z",
+		},
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.True(doesMatch)
+}
+func TestMultipleMatchesRequiredNested(t *testing.T) {
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"a": "b",
+		"x": map[string]interface{}{
+			"y": "z",
+		},
+	}
+	match := map[string]interface{}{
+		"x": map[string]interface{}{
+			"y": "z",
+		},
+		"a": "b",
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.True(doesMatch)
+}
+
+//array in message, scalar in match: checks membership
+func TestArrayContains(t *testing.T) {
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"a": []interface{}{1, 2, 3},
+	}
+	match := map[string]interface{}{
+		"a": 2,
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.True(doesMatch)
+}
+func TestArrayDoesNotContain(t *testing.T) {
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"a": []interface{}{1, 2, 3},
+	}
+	match := map[string]interface{}{
+		"a": 5,
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.False(doesMatch)
+}
+
+//array on both sides: full recursion
+func TestArrayFullMatch(t *testing.T) {
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"a": []interface{}{1, 2, 3},
+	}
+	match := map[string]interface{}{
+		"a": []interface{}{1, 2, 3},
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.True(doesMatch)
+}
+func TestNestedArrayFullMatch(t *testing.T) {
+	assert := assert.New(t)
+	firstSub := map[string]interface{}{
+		"a": "b",
+	}
+	secondSub := map[string]interface{}{
+		"a": "b",
+	}
+	message := map[string]interface{}{
+		"a": []interface{}{firstSub, 2, 3},
+	}
+	match := map[string]interface{}{
+		"a": []interface{}{secondSub, 2, 3},
+	}
+	doesMatch, matchErr := Match(message, match)
+	assert.Nil(matchErr)
+	assert.True(doesMatch)
+}
+
+/* legacy
 //func Match(message map[string]interface{}, match map[string]interface{}) (bool, error) {
 func TestBasicMatchStringAndNumber(t *testing.T) {
 	assert := assert.New(t)
@@ -116,3 +284,4 @@ func TestBasicMatch(t *testing.T) {
 	assert.True(doesMatch)
 	pretty.Print("noop")
 }
+*/
--- tmp/small_test_thing.go
+++ tmp/small_test_thing.go
@@ -7,22 +7,24 @@ import (
 
 //func Match(message map[string]interface{}, match map[string]interface{}) (bool, error) {
 func main() {
+	x := []interface{}{"foo"}
 	message := map[string]interface{}{
 		"Name": "Wednesday",
 		"Age":  6,
 		"Parents": map[string]interface{}{
 			"bee": "boo",
 			"foo": map[string]interface{}{
-				"hi": []interface{}{"a", "b"},
+				"hi": []interface{}{"a", "b", x},
 			},
 		},
 	}
+	pretty.Println(message)
 	match := map[string]interface{}{
 		"Name": "Wednesday",
 		"Age":  6,
 	}
 
 	doesMatch, matchErr := messagematch.Match(message, match)
-	pretty.Print(matchErr)
-	pretty.Print(doesMatch)
+	pretty.Println(matchErr)
+	pretty.Println(doesMatch)
 }
